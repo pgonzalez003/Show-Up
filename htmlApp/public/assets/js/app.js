7a42541fa44e87f03dab4e96b9c241fe682e8d5c
Pat Gonzalez <pgonzalez003@gmail.com>
2019-08-16 00:03:34 -0700

    added file structure for database and server, also added js functions and dynamic buttons

---
+++ htmlApp/public/assets/js/app.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+    $('.parallax').parallax();
+    $('select').formSelect();
+    $('#indexSubmit').on("click", function(){
+        var workoutValue = $('#workoutSelect').find(':selected').val();
+        document.location.href = workoutValue;
+        $('.name').text(nameValue);
+    })
+
+    var exercise = [
+        {
+            area:"Chest", 
+            motion:["Bench", " Incline Press", " Incline Fly"]
+        },
+        {
+            area:"Back", 
+            motion:["Row", " Lat Pull", " Standing Row", " Pull"]
+        },
+        {
+            area: "Bicep",
+            motion: ["Standing Curl", " Incline Curl", " Chin Up"]
+        },
+        {
+            area: "Tricep",
+            motion: ["Extension", " Dips", " Skull Crusher"]
+        },
+        {
+            area: "Shoulder",
+            motion: ["Pull Up", " Military Press", " Rack Pull", " Lateral Raise", " Reverse Fly"]
+        },
+        {
+            area:"Lower", 
+            motion:"Squat"
+        },
+    ];
+
+   for (var i = 0; i < exercise.length; i++) {
+        console.log(exercise[i].motion.length   );
+        let button = $("<a>");
+        button.addClass("btn-floating btn-large waves-effect waves-light red");
+        button.attr("id", "areaButton");
+        button.attr("data-letter", exercise[i].motion);
+        button.text(exercise[i].area);
+        
+
+        $(".buttonLanding").append(button);
+    }
+
+    $(".btn-floating").on("click", function(){
+        // let motionButton = $("<a>");
+        // motionButton.addClass("btn-floating btn-small waves-effect waves-light red");
+        // motionButton.attr("id", "motionButton");
+        // motionButton.attr("data-motion", exercise.motion[i]);
+        // motionButton.text(exercise[i].motion.length);
+        // $(".textLanding").append(motionButton);
+        let textDisplay = $("<div>");
+        textDisplay.text($(this).attr("data-letter"));
+        $(".textLanding").append(textDisplay);
+    })
+
+    // $("#motionButton").on("click", function(){
+    //     let textDisplay = $("<div>");
+    //     textDisplay.text($(this).attr("data-motion"));
+    //     $(".motionLanding").append(textDisplay);
+    // })
+
+    $("#delete").on("click", function(){
+        $(".textLanding").empty();
+    })
+
+    // $("#save").on("click", function(){
+    //     $(".saveLanding").append($(".textLanding"))
+    // })
+
+
+});
+
